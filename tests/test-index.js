e351420b5df9b1c9d0261c0205b89327f9585a0f
kanhaiya <kanhaiyas@lambdatest.com>
2019-05-01 19:19:40 +0530

    Update test case

--- tests/test-index.js
+++ tests/test-index.js
@@ -1,8 +1,27 @@
 var chai = require('chai');
 var expect = chai.expect;
-var localTunnelConfig = require('../lib/conf/node-tunnel-config.json');
-describe('Tunnel Test', function() {
-  it('File for config tunnel is exist ? ', function() {
-    expect(typeof localTunnelConfig).to.equal('object');
+describe('Tunnel runs successfully', function() {
+  it('Tunnel doesnt start on wrong parameter', function() {
+    var isTunnelStarted = false;
+    before(function() {
+      this.timeout('10sec');
+      //creates an instance of Tunnel
+      var tunnelInstance = new lambdaTunnel();
+      // replace <lambdatest-user> with your user and <lambdatest-accesskey> with your key.
+      var tunnelArguments = {
+        user: process.env.LT_USERNAME || '<lambdatest-user>',
+        key: process.env.LT_ACCESS_KEY || '<lambdatest-accesskey>'
+      };
+      try {
+        tunnelInstance.start(tunnelArguments, function(error, status) {
+          if (!error) {
+            isTunnelStarted = true;
+          }
+        });
+      } catch (error) {
+        isTunnelStarted = false;
+      }
+    });
+    expect(typeof isTunnelStarted).to.equal('boolean');
   });
 });
