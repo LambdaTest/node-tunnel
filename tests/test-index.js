69b5e683226fe62df8252e1e32938da8a3978d87
kanhaiya <kanhaiyas@lambdatest.com>
2019-05-01 18:22:54 +0530

    Add Support for callback, promise, async/await

--- tests/test-index.js
+++ tests/test-index.js
@@ -1,24 +1,8 @@
 var chai = require('chai');
-var lambdaTunnel = require('../lib/tunnel');
 var expect = chai.expect;
-describe('Tunnel Instance', function() {
-  it('Is Tunnel Instance is Running ?', function() {
-    var isTunnelStarted = false;
-    before(async function() {
-      this.timeout('10sec');
-      //creates an instance of Tunnel
-      var tunnelInstance = new lambdaTunnel();
-      // replace <lambdatest-user> with your user and <lambdatest-accesskey> with your key.
-      var tunnelArguments = {
-        user: process.env.LT_USERNAME || '<lambdatest-user>',
-        key: process.env.LT_ACCESS_KEY || '<lambdatest-accesskey>'
-      };
-      try {
-        isTunnelStarted = await tunnelInstance.start(tunnelArguments);
-      } catch (error) {
-        isTunnelStarted = false;
-      }
-    });
-    expect(typeof isTunnelStarted).to.equal('boolean');
+var localTunnelConfig = require('../lib/conf/node-tunnel-config.json');
+describe('Tunnel Test', function() {
+  it('File for config tunnel is exist ? ', function() {
+    expect(typeof localTunnelConfig).to.equal('object');
   });
 });
