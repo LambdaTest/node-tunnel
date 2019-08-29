6c480e5cc3d91cec63e2e6e25657301c91966622
kanhaiya <kanhaiyas@lambdatest.com>
2019-08-29 13:10:11 +0530

    Fix for Stage test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('./lib/tunnel');
+const LambdatTestTunnel = require('./lib/tunnel');
+const tunnelInstance = new LambdatTestTunnel();
+const tunnelArguments = {
+  user: '<Username>',
+  key: '<AccessKey>',
+  env: 'stage'
+};
+(async function() {
+  try {
+    await tunnelInstance.start(tunnelArguments);
+    console.log('Tunnel is Running Successfully');
+  } catch (err) {
+    console.log(err);
+  }
+})();
