5dcd18bb3d18428882efce1c565e2e4286fc67c9
kanhaiya <kanhaiyas@lambdatest.com>
2020-03-23 15:21:48 +0530

    Fix for deprecation and added index.d.ts file for module declaration

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+declare module "@lambdatest/node-tunnel" {
+    interface Options {
+      user: string;
+      key: string;
+      port: string;
+      tunnelName: string;
+      proxyHost: string;
+      proxyPort: string;
+      proxyUser: string;
+      proxyPass: string;
+      localDir: string;
+      environment: boolean;
+      verbose: string;
+      configFile: string;
+      sharedTunnel: string;
+      localDomains: string;
+      outputConfig: string;
+      dns: string;
+      pidFile: string;
+      pac: string;
+      logFile: string;
+      controller: string;
+      [key: string]: string | boolean;
+    }
+  
+    class Tunnel {
+      start(options: Partial<Options>, callback: (error?: Error) => void): void;
+      isRunning(): boolean;
+      stop(callback: () => void): void;
+    }
+  }
+  
