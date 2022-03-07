d39742b690c327a91b8c96c16643254b6427479c
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2022-03-07 19:41:15 +0530

    added testfile

--- tests/test-index.js
+++ tests/test-index.js
@@ -1,27 +1,31 @@
+var lambdaTunnel = require('../index')
 var chai = require('chai');
 var expect = chai.expect;
+
 describe('Tunnel runs successfully', function() {
+  var isTunnelStarted = false;
+  beforeEach(function(done) {
+    //start tunnel
+    this.timeout(10000)
+    var tunnelInstance = new lambdaTunnel();
+    // replace <lambdatest-user> with your user and <lambdatest-accesskey> with your key.
+    var tunnelArguments = {
+      user: process.env.LT_USERNAME || '<lambdatest-user>',
+      key: process.env.LT_ACCESS_KEY || '<lambdatest-accesskey>'
+    };
+    try {
+      tunnelInstance.start(tunnelArguments, function(error, status) {
+        if (!error) {
+          isTunnelStarted = true;
+          done()
+        }
+      });
+    } catch (error) {
+      isTunnelStarted = false;
+    }
+  })
   it('Tunnel doesnt start on wrong parameter', function() {
-    var isTunnelStarted = false;
-    before(function() {
-      this.timeout('10sec');
-      //creates an instance of Tunnel
-      var tunnelInstance = new lambdaTunnel();
-      // replace <lambdatest-user> with your user and <lambdatest-accesskey> with your key.
-      var tunnelArguments = {
-        user: process.env.LT_USERNAME || '<lambdatest-user>',
-        key: process.env.LT_ACCESS_KEY || '<lambdatest-accesskey>'
-      };
-      try {
-        tunnelInstance.start(tunnelArguments, function(error, status) {
-          if (!error) {
-            isTunnelStarted = true;
-          }
-        });
-      } catch (error) {
-        isTunnelStarted = false;
-      }
-    });
     expect(typeof isTunnelStarted).to.equal('boolean');
+    expect(isTunnelStarted).to.equal(true);
   });
 });
