b3c9bd796a87a17a15a514ddead3b3d30533fbf0
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2022-03-08 13:04:58 +0530

    added test

--- tests/test-index.js
+++ tests/test-index.js
@@ -1,4 +1,4 @@
-var lambdaTunnel = require('../index')
+var lambdaTunnel = require('../index');
 var chai = require('chai');
 var expect = chai.expect;
 
@@ -6,7 +6,7 @@ describe('Tunnel runs successfully', function() {
   var isTunnelStarted = false;
   beforeEach(function(done) {
     //start tunnel
-    this.timeout(10000)
+    this.timeout(10000);
     var tunnelInstance = new lambdaTunnel();
     // replace <lambdatest-user> with your user and <lambdatest-accesskey> with your key.
     var tunnelArguments = {
@@ -17,13 +17,13 @@ describe('Tunnel runs successfully', function() {
       tunnelInstance.start(tunnelArguments, function(error, status) {
         if (!error) {
           isTunnelStarted = true;
-          done()
+          done();
         }
       });
     } catch (error) {
       isTunnelStarted = false;
     }
-  })
+  });
   it('Tunnel doesnt start on wrong parameter', function() {
     expect(typeof isTunnelStarted).to.equal('boolean');
     expect(isTunnelStarted).to.equal(true);
